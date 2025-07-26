e99b5f3386e7df20024f26938032c1f06dcc20c6
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-07-26 02:13:14 -0700

    Add auth route handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const expressSession = require("express-session");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const prisma = require('./prisma.js');
 const passport = require("passport");
+const authRouter = require("./routes/auth.js");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -28,6 +29,17 @@ app.use(
     )
   })
 );
+
+app.use("/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>404! Page not found</h1>");
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
---
+++ routes/auth.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+const prisma = require("../prisma.js");
+
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+router.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+router.post(
+  "/sign-up",
+  body("confirm")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords do not match!"),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("sign-up-form", { errors: errors.array() });
+    }
+
+    try {
+      const existingUser = await prisma.user.findUnique({
+        where: { username: req.body.username },
+      });
+      if (existingUser) {
+        return res.status(400).render("sign-up-form", {
+          errors: [{ msg: "Username already taken" }],
+        });
+      }
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const user = await prisma.user.create({
+        data: {
+          username: req.body.username,
+          password: hashedPassword,
+        },
+      });
+      res.redirect("/");
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  }
+);
+
+router.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
