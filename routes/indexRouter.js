3c886b73aada979d52826bfd62a0500e77a72554
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-07-30 01:51:58 -0700

    Add display of folders and files on index page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,34 @@
-const express = require('express');
+const express = require("express");
+const prisma = require("../prisma");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res, next) => {
+  try {
+    if (req.isAuthenticated()) {
+      const [folders, loneFiles] = await Promise.all([
+        prisma.folder.findMany({
+          where: {
+            authorId: req.user.id,
+          },
+        }),
+        prisma.file.findMany({
+          where: {
+            authorId: req.user.id,
+            folderId: null,
+          },
+        }),
+      ]);
+
+      return res.render("index", {
+        folders,
+        loneFiles,
+      });
+    } else {
+      return res.render("index", { folders: [], loneFiles: [] });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
